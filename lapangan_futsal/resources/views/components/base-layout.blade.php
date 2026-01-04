64926541bca30de9258491b74beea80754447bdb
FarhanSaleh <anak.upload90@gmail.com>
2026-01-04 16:00:34 +0800

    feat: update navbar

--- lapangan_futsal/resources/views/components/base-layout.blade.php
+++ lapangan_futsal/resources/views/components/base-layout.blade.php
@@ -10,76 +10,191 @@
 </head>
 
 <body>
-    <div>
-        <nav>
-            <ul class="flex gap-2">
+    <div class="drawer">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col">
+            <nav class="sticky top-0 navbar bg-base-100 shadow-sm">
+                <div class="flex-none lg:hidden">
+                    <label for="my-drawer-2" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="inline-block h-6 w-6 stroke-current">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16">
+                            </path>
+                        </svg>
+                    </label>
+                </div>
+                <div class="flex-1">
+                    <a class="btn btn-ghost text-xl">Lapangan Futsal</a>
+                </div>
+                <div class="hidden flex-none lg:block">
+                    <ul class="menu menu-horizontal px-1">
+                        {{-- ADMIN --}}
+                        @if (auth()->user()->hasRole('admin'))
+                        <li>
+                            <a href="/dashboard"
+                                class="{{ request()->is('dashboard') ? 'menu-active' : '' }}">dashboard</a>
+                        </li>
+                        <li>
+                            <a href="/users" class="{{ request()->is('users*') ? 'menu-active' : '' }}">users</a>
+                        </li>
+                        <li>
+                            <a href="/fields" class="{{ request()->is('fields*') ? 'menu-active' : '' }}">lapangan</a>
+                        </li>
+                        <li>
+                            <a href="/schedules"
+                                class="{{ request()->is('schedules*') ? 'menu-active' : '' }}">jadwal</a>
+                        </li>
+                        <li>
+                            <a href="/reservations"
+                                class="{{ request()->is('reservations*') ? 'menu-active' : '' }}">reservasi</a>
+                        </li>
+                        <li>
+                            <a href="/activity-log"
+                                class="{{ request()->is('activity-log') ? 'menu-active' : '' }}">activity
+                                log</a>
+                        </li>
+                        <li>
+                            <a href="/profile" class="{{ request()->is('profile') ? 'menu-active' : '' }}">profile</a>
+                        </li>
+                        @endif
+
+                        {{-- PENGELOLA --}}
+                        @if (auth()->user()->hasRole('pengelola'))
+                        <li>
+                            <a href="/dashboard"
+                                class="{{ request()->is('dashboard') ? 'menu-active' : '' }}">dashboard</a>
+                        </li>
+                        <li>
+                            <a href="/fields" class="{{ request()->is('fields*') ? 'menu-active' : '' }}">lapangan</a>
+                        </li>
+                        <li>
+                            <a href="/schedules"
+                                class="{{ request()->is('schedules*') ? 'menu-active' : '' }}">jadwal</a>
+                        </li>
+                        <li>
+                            <a href="/reservations"
+                                class="{{ request()->is('reservations*') ? 'menu-active' : '' }}">reservasi</a>
+                        </li>
+                        <li>
+                            <a href="/profile" class="{{ request()->is('profile') ? 'menu-active' : '' }}">profile</a>
+                        </li>
+                        @endif
+
+                        {{-- USER --}}
+                        @if (auth()->user()->hasRole('user'))
+                        <li>
+                            <a href="/dashboard"
+                                class="{{ request()->is('dashboard') ? 'menu-active' : '' }}">dashboard</a>
+                        </li>
+                        <li>
+                            <a href="/fields" class="{{ request()->is('fields*') ? 'menu-active' : '' }}">lapangan</a>
+                        </li>
+                        <li>
+                            <a href="/schedules"
+                                class="{{ request()->is('schedules*') ? 'menu-active' : '' }}">jadwal</a>
+                        </li>
+                        <li>
+                            <a href="/reservations/my"
+                                class="{{ request()->is('reservations*') ? 'menu-active' : '' }}">reservasi</a>
+                        </li>
+                        <li>
+                            <a href="/profile" class="{{ request()->is('profile') ? 'menu-active' : '' }}">profile</a>
+                        </li>
+                        @endif
+                        <form action="/logout" method="POST">
+                            <li>
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit" class="hover:bg-red-500 hover:text-white">Logout</button>
+                            </li>
+                        </form>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+            <ul class="menu bg-base-200 min-h-full w-80 p-4">
                 {{-- ADMIN --}}
                 @if (auth()->user()->hasRole('admin'))
-                <li class="border p-1">
-                    <a href="/dashboard">dashboard</a>
+                <li>
+                    <a href="/dashboard" class="{{ request()->is('dashboard') ? 'menu-active' : '' }}">dashboard</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/users">users</a>
+                <li>
+                    <a href="/users" class="{{ request()->is('users*') ? 'menu-active' : '' }}">users</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/fields">lapangan</a>
+                <li>
+                    <a href="/fields" class="{{ request()->is('fields*') ? 'menu-active' : '' }}">lapangan</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/schedules">jadwal</a>
+                <li>
+                    <a href="/schedules" class="{{ request()->is('schedules*') ? 'menu-active' : '' }}">jadwal</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/reservations">reservasi</a>
+                <li>
+                    <a href="/reservations"
+                        class="{{ request()->is('reservations*') ? 'menu-active' : '' }}">reservasi</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/activity-log">activity log</a>
+                <li>
+                    <a href="/activity-log" class="{{ request()->is('activity-log') ? 'menu-active' : '' }}">activity
+                        log</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/profile">profile</a>
+                <li>
+                    <a href="/profile" class="{{ request()->is('profile') ? 'menu-active' : '' }}">profile</a>
                 </li>
                 @endif
 
                 {{-- PENGELOLA --}}
                 @if (auth()->user()->hasRole('pengelola'))
-                <li class="border p-1">
-                    <a href="/dashboard">dashboard</a>
+                <li>
+                    <a href="/dashboard" class="{{ request()->is('dashboard') ? 'menu-active' : '' }}">dashboard</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/fields">lapangan</a>
+                <li>
+                    <a href="/fields" class="{{ request()->is('fields*') ? 'menu-active' : '' }}">lapangan</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/schedules">jadwal</a>
+                <li>
+                    <a href="/schedules" class="{{ request()->is('schedules*') ? 'menu-active' : '' }}">jadwal</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/reservations">reservasi</a>
+                <li>
+                    <a href="/reservations"
+                        class="{{ request()->is('reservations*') ? 'menu-active' : '' }}">reservasi</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/profile">profile</a>
+                <li>
+                    <a href="/profile" class="{{ request()->is('profile') ? 'menu-active' : '' }}">profile</a>
                 </li>
                 @endif
 
                 {{-- USER --}}
                 @if (auth()->user()->hasRole('user'))
-                <li class="border p-1">
-                    <a href="/dashboard">dashboard</a>
+                <li>
+                    <a href="/dashboard" class="{{ request()->is('dashboard') ? 'menu-active' : '' }}">dashboard</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/fields">lapangan</a>
+                <li>
+                    <a href="/fields" class="{{ request()->is('fields*') ? 'menu-active' : '' }}">lapangan</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/schedules">jadwal</a>
+                <li>
+                    <a href="/schedules" class="{{ request()->is('schedules*') ? 'menu-active' : '' }}">jadwal</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/reservations/my">reservasi</a>
+                <li>
+                    <a href="/reservations/my"
+                        class="{{ request()->is('reservations*') ? 'menu-active' : '' }}">reservasi</a>
                 </li>
-                <li class="border p-1">
-                    <a href="/profile">profile</a>
+                <li>
+                    <a href="/profile" class="{{ request()->is('profile') ? 'menu-active' : '' }}">profile</a>
                 </li>
                 @endif
+                <form action="/logout" method="POST">
+                    <li>
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="hover:bg-red-500 hover:text-white">Logout</button>
+                    </li>
+                </form>
             </ul>
-        </nav>
-        <br>
-        {{ $slot }}
+        </div>
     </div>
+    <main class="container mx-auto mt-6 px-2">
+        {{ $slot }}
+    </main>
 </body>
 
 </html>
--- lapangan_futsal/resources/views/tes/dashboard.blade.php
+++ lapangan_futsal/resources/views/tes/dashboard.blade.php
@@ -1,8 +1,3 @@
 <x-base-layout>
     <h1 class="text-2xl">Dashbaord</h1>
-    <form action="/logout" method="POST">
-        @csrf
-        @method("DELETE")
-        <button type="submit" class="border">Logout</button>
-    </form>
 </x-base-layout>
